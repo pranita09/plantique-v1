bece5db43f6f74a803def187981060c2f190d299
Pranita <pfulsundar8@gmail.com>
2023-05-16 05:47:31 +0530

    feat: filters component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import {Routes, Route} from 'react-router-dom';
@@ -28,7 +27,7 @@ function App() {
         <Route path='/mockman' element={<MockMan />} />
         <Route path='/*' element={<PageNotFound/>} />
       </Routes>
-      <Footer />
+
     </div>
   );
 }
--- src/pages/ProductListing/ProductListing.jsx
+++ src/pages/ProductListing/ProductListing.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import './ProductListing.css';
+import FiltersAside from './components/FiltersAside/FiltersAside';
+import FiltersDrawer from './components/FiltersDrawer/FiltersDrawer';
+import FiltersFooter from './components/FiltersFooter/FiltersFooter';
 
 const ProductListing = () =>{
+    const [open, setOpen] = useState(false);
     return(
-        <>
-            <h1>Product Listing Page</h1>
+        <div className='product-listing'>
+            {<FiltersAside />}
+            <FiltersDrawer open={open} setOpen={setOpen}/>
             <ProductCard />
-        </>
+            <FiltersFooter setOpen={setOpen} />
+        </div>
     )
 }
 
