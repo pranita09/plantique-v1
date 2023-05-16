e7d6c22a6622ca916ebc830b4d70cef8637d3dab
Pranita <pfulsundar8@gmail.com>
2023-05-16 14:30:14 +0530

    fix: Category component

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,10 +1,13 @@
 import './Categories.css';
 import { categories } from '../../backend/db/categories';
 
-const CategoryCard = ({catImg}) =>{
+const CategoryCard = ({catImg, category}) =>{
     return(
-        <div className='category-card'>
-            <img src={catImg} alt='category-img'/>
+        <div className='category-card-container'>
+            <div className='category-card'>
+                <img src={catImg} alt='category-img'/>
+            </div>
+            <p>{category}</p>
         </div>
     )
 }
@@ -12,13 +15,13 @@ const CategoryCard = ({catImg}) =>{
 const Categories = () =>{
     return(
         <>
-            <div className="category-container">
-                <h1>Gardening</h1>
-                <p>Gardening is not just a activity - for some, it is a stress release. For others, it is an escape into a world filled with hope and joy.</p>
+            <div className="category-outer-container">
+                <h1>What are you looking for?</h1>
+                <p>Gardening is not just a activity - for some, it is a stress release. For others, it is an escape into a world filled with hope and joy. So, choose your plants now!</p>
                 <div className='category-cards'>
                     {
-                        categories.map(({_id, img}) => (
-                            <CategoryCard key={_id} catImg={img}/>
+                        categories.map(({_id, img, categoryName}) => (
+                            <CategoryCard key={_id} catImg={img} category={categoryName}/>
                         ))
                     }
                 </div>
