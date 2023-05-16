580311468ee9ceaa828d79e249cdb6a38ed52abc
Pranita <pfulsundar8@gmail.com>
2023-05-16 13:39:07 +0530

    fix: dataset

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,22 +8,37 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    img: "/images/category-img-1.jpg",
+    img: "/images/categories/category-img-1.jpg",
     categoryName: "Flowers"
   },
   {
     _id: uuid(),
-    img: "/images/category-img-2.jpg",
+    img: "/images/categories/category-img-2.jpg",
     categoryName: "Indoor"
   },
   {
     _id: uuid(),
-    img: "/images/category-img-3.jpg",
+    img: "/images/categories/category-img-3.jpg",
     categoryName: "Outdoor"
   },
   {
     _id: uuid(),
-    img: "/images/category-img-4.jpg",
-    categoryName: "For Office Desk"
+    img: "/images/categories/category-img-4.jpg",
+    categoryName: "For TableTop"
+  },
+  {
+    _id: uuid(),
+    img: "/images/categories/category-img-5.jpg",
+    categoryName: "Medicinal"
+  },
+  {
+    _id: uuid(),
+    img: "/images/categories/category-img-6.jpg",
+    categoryName: "Climbers"
+  },
+  {
+    _id: uuid(),
+    img: "/images/categories/category-img-7.jpg",
+    categoryName: "Fruits"
   },
 ];
