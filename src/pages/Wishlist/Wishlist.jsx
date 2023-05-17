f100ea2d02e40f48c4f3741606db0f63900d8171
Pranita <pfulsundar8@gmail.com>
2023-05-18 03:58:58 +0530

    fix: loader styling

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,13 +1,9 @@
-import PageNotFound from '../PageNotFound/PageNotFound';
-import Loader from '../../components/Loader/Loader'
 import './Wishlist.css';
 
 const Wishlist = () =>{
     return(
         <>
-            {/* <h1>Wshlist Page</h1> */}
-            <Loader />
-            <PageNotFound />
+            <h1>Wishlist Page</h1>
         </>
     )
 }
