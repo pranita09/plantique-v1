923eddfd616a935b4cf3c2f78ede49fd64d5af98
Pranita <pfulsundar8@gmail.com>
2023-05-19 12:02:35 +0530

    feat: UI of Cart page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,13 +1,40 @@
+import { Link } from 'react-router-dom';
 import CartCard from '../../components/CartCard/CartCard';
 import CartPrice from '../../components/CartPrice/CartPrice';
 import './Cart.css';
 
 const Cart = () =>{
+
+    const cartState = [1];
+
     return(
-        <>
-            {/* <CartCard /> */}
-            <CartPrice />
-        </>
+        <div className='page-wrapper'>
+            <section className="cart-container">
+                <div className='cart-container-heading'><h2>My Cart (<span>{cartState.length}</span>)</h2></div>
+
+                {
+                    cartState.length > 0 ? (
+                        <div className='cart-main'>
+                            <div className='cart-products'>
+                                <CartCard />
+                                {/* <CartCard /> */}
+                                {/* <CartCard /> */}
+                            </div>
+                            <div className='cart-price'>
+                                <CartPrice />
+                            </div>
+                        </div>
+                    ) : 
+                    (<div className='text-center'>
+                        <p>Oops! Your cart is empty.</p>
+                        <button><Link to="/store" className='cart-to-store-link'>
+                            Explore the store.
+                        </Link></button>
+                    </div>)
+                }
+
+            </section>
+        </div>
     )
 }
 
