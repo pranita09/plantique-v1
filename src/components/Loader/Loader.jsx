da39e13818a3042fd64e843b8071b7503288d480
Pranita <pfulsundar8@gmail.com>
2023-05-27 04:16:09 +0530

    fix: Loader component and its position

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import "./Loader.css";
 const Loader = () =>{
     return(
         <div className="loader">
-            <img src="/assets/loader.svg" alt="loader"/>
+            <img src="/assets/spin-loader.svg" alt="loader"/>
         </div>
     )
 }
