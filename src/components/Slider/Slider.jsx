f33b9da7d8f79a32d0947da7bfe89166a64d2cd1
Pranita <pfulsundar8@gmail.com>
2023-05-30 00:30:21 +0530

    fix: minor fix

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 import "./Slider.css";
 
 const slides = [
-    "/images/banner-img-3.jpg",
   "https://cdn.shopify.com/s/files/1/0047/9730/0847/files/nurserylive-home-page-banner-combo-pack-v3_1346x499.jpg?v=1636743217",
   "https://cdn.shopify.com/s/files/1/0047/9730/0847/files/nurserylive-home-page-cactus-and-succulent-banner-v3_6f9a298f-e6db-4a5c-9357-2a35f512f084_1346x499.jpg?v=1637848242",
   "https://cdn.shopify.com/s/files/1/0047/9730/0847/files/nurserylive-home-page-banner-balcony-and-terrace-garden-metal-stand-v3_4976c128-13ab-4328-b195-64b88639692a_1349x500.jpg?v=1636743197",
+  "/images/banner-img-3.jpg",
 ];
 
 function Slider() {
