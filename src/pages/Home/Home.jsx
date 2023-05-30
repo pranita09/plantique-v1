f5e76fb8274a08522f94d1d49ab2b11d4417eebe
Pranita <pfulsundar8@gmail.com>
2023-05-30 18:20:17 +0530

    fix: document title and favicon

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Slider from "../../components/Slider/Slider";
 import Categories from "../../components/Categories/Categories";
 
 function Home() {
+  document.title = "Plantique Store";
   return (
     <>
       <Slider />
